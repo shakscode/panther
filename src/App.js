9f3f5cb535bd27967d6328c4cd50ce220b67164b
shakscode <53089845+shakscode@users.noreply.github.com>
2020-01-26 21:51:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>**************MISSION PLAYBOY 2020*******************</code> and save to reload.
+          MISSION PLAYBOY 2020
         </p>
         <a
           className="App-link"
